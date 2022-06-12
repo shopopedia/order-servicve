214964ccbded06ee0a141bfced1f4ecfcf76c4f4
parishkar-nagrikar <p4parish@gmail.com>
2022-06-13 00:08:32 +0530

    first commit

---
+++ src/test/java/com/shopoedia/order/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopoedia.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
